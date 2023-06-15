43e437d039d1f1687e97e416ed7d0db82a505374
Depak Borhara <depakborhara@gmail.com>
2023-06-15 12:28:02 -0400

    Added slides for React and updated order of assignments

--- playground.js
+++ playground.js
@@ -1,7 +1,15 @@
-let array = [1, 2, 3];
+//SPREAD
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+// let arr2a = [arr1, 4, 5, 6];
+// let arr3 = arr1.concat(arr2);
+// console.log(arr2a);
 
-array.forEach(function (ele) {
-  console.log(ele);
-});
+//REST
+function checkRest(...rest) {
+  //   console.log("a", a);
+  //   console.log("b", b);
+  rest.forEach((element) => console.log(element));
+}
 
-array.forEach((ele) => console.log(ele));
+console.log(checkRest(1, 2, 3, 4, 5, 6, 100, 300));
