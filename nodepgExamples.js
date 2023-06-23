9d7c654bdf04a16e7c511abdbd141a908b5666e4
Depak Borhara <depakborhara@gmail.com>
2023-06-23 12:16:43 -0400

    node example and express slides

---
+++ nodepgExamples.js
@@ -0,0 +1,34 @@
+// Include the node-postgres library
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: "localhost",
+  port: 5432,
+  database: "backendscaffold",
+  user: "depakborhara",
+  password: "",
+});
+
+// READ FROM POSTGRESQL FROM NODE
+pool.query("SELECT * FROM players", (error, result) => {
+  if (error) {
+    console.error("Error executing query", error);
+  } else {
+    console.log("Query result:", result.rows);
+  }
+});
+
+// WRITE TO POSTGRESQL FROM NODE
+// pool.query(
+//   `INSERT INTO players
+//   ("firstName", "lastName", "jerseyNumber", "createdAt", "updatedAt")
+//   VALUES
+//   ('Sherlock', 'Holmes', '4', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)`,
+//   (err, result) => {
+//     if (err) {
+//       console.error("Error executing query", err);
+//     } else {
+//       console.log("Query result:", result.rows);
+//     }
+//   }
+// );
